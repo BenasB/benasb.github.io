9f6671433f8413f3b4609c6e0c28bf6cb798719b
benasb-bankera <71316641+benasb-bankera@users.noreply.github.com>
2021-04-05 13:01:29 +0300

    Finalized setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,9 @@ import image from './react.svg';
 import Counter from './Counter';
 
 export const App: React.FC = () => {
-  const name = 'Benas Test';
   return (
     <>
-      <h1>
-        Hello World! - {process.env.NODE_ENV} - Name: {name}
-      </h1>
+      <h1>Hello World! - {process.env.NODE_ENV}</h1>
       <img src={image} alt={'React logo'} width={300} />
       <Counter />
     </>
