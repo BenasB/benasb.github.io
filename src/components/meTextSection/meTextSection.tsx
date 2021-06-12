e00d2c8ce7c1b940f707cb70f9066f94f1192144
BenasB <29711974+BenasB@users.noreply.github.com>
2021-06-12 22:08:48 +0300

    Reworked meTextSection.tsx component

--- src/components/meTextSection/meTextSection.tsx
+++ src/components/meTextSection/meTextSection.tsx
@@ -2,20 +2,13 @@ import style from './meTextSection.module.scss';
 
 interface Props {
   title: string;
-  text: string[];
 }
 
-const MeTextSection: React.FC<Props> = ({ title, text }) => {
+const MeTextSection: React.FC<Props> = ({ title, children }) => {
   return (
     <>
       <h2 className={style.title}>{title}</h2>
-      {text.map((t, key) => {
-        return (
-          <p className={style.text} key={key}>
-            {t}
-          </p>
-        );
-      })}
+      <div className={style.text}>{children}</div>
     </>
   );
 };
