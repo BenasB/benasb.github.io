4e57b53668c9bb8940133ce6b0fc9a3ca19fee6f
BenasB <29711974+BenasB@users.noreply.github.com>
2022-12-27 19:38:44 +0200

    Fix anchor scrolling

--- src/utils/ScrollToTop.tsx
+++ src/utils/ScrollToTop.tsx
@@ -2,11 +2,17 @@ import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop: React.FC = ({ children }) => {
-  const location = useLocation();
+  const { pathname, hash, key } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-  }, [location.pathname]);
+    if (hash === '') window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    else
+      setTimeout(() => {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        if (element) element.scrollIntoView();
+      }, 0);
+  }, [pathname, hash, key]);
 
   return <>{children}</>;
 };
